97b61fa55f97b7d99cdb306a021ce2454dfa17e7
segun <soshice@gmail.com>
2022-11-04 14:20:56 +0100

    Form validation commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,53 +1,137 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
 const Contact = () => {
+  const [value, setValue] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+  useEffect(() => {
+    setTimeout(() => {
+      setErrorMessage("");
+    }, 2000);
+  }, [errorMessage]);
+
+  function changeEvent(e) {
+    let name = e.name;
+    let value = e.value;
+
+    setValue((curr) => {
+      return {
+        ...curr,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitEvent(e) {
+    e.preventDefault();
+    if (!value.firstname) setErrorMessage("firstname");
+    else if (!value.lastname) setErrorMessage("lastname");
+    else if (!value.email) setErrorMessage("email");
+    else if (!value.message) setErrorMessage("message");
+  }
+
+  console.log(errorMessage, value.firstname);
   return (
-    <div className='form-container'>
-        <div id='heading'>
-            <h3>
-                Contact Me
-            </h3>
+    <div className="form-container">
+      <div id="heading">
+        <h3>Contact Me</h3>
 
-            <p>
-                Hi there, contact me and drop me a message 
+        <p>Hi there, contact me and drop me a message</p>
+      </div>
+      <form onSubmit={submitEvent}>
+        <div className="names-input">
+          <div className="text-input">
+            <label htmlFor="firstname">Firstname: </label>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="enter your firstname"
+              id="first_name"
+              value={value.firstname}
+              onChange={(e) => changeEvent(e.target)}
+            />
+            <p className="error">
+              {errorMessage &&
+                errorMessage === "firstname" &&
+                `firstname can't be empty`}
             </p>
-        </div>
-        <form>
-         <div className='names-input'>
-         <div className='text-input'>
-                <label htmlFor="firstname">Firstname: </label>
-                <input type="text" name='firstname' placeholder='enter your firstname' id='first_name'  required/>
-            </div>
+          </div>
 
-            <div className='text-input'>
+          <div className="text-input">
             <label htmlFor="Lastname">Lastname: </label>
-                <input type="text" name='Lastname' placeholder='enter your lastname' id='last_name'  required/>
-            </div>
-         </div>
+            <input
+              type="text"
+              name="lastname"
+              placeholder="enter your lastname"
+              id="last_name"
+              value={value.lastname}
+              onChange={(e) => changeEvent(e.target)}
+            />
+            <p className="error">
+              {errorMessage &&
+                errorMessage === "lastname" &&
+                `lastname can't be empty`}
+            </p>
+          </div>
+        </div>
 
-      <div className='email-msg'>
-      <div className='text-input'>
+        <div className="email-msg">
+          <div className="text-input">
             <label htmlFor="email">Email:</label>
 
-                <input type="email" placeholder='example@email.com' id='email' required/>
-            </div>
+            <input
+              type="email"
+              name="email"
+              placeholder="example@email.com"
+              id="email"
+              value={value.email}
+              onChange={(e) => changeEvent(e.target)}
+            />
+              <p className="error">
+              {errorMessage &&
+                errorMessage === "email" &&
+                `email can't be empty`}
+            </p>
+          </div>
 
-            <div className='text-input'>
+          <div className="text-input">
             <label htmlFor="message">Message:</label>
 
-                <textarea name="" id="message" cols="70" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
-            </div>
-      </div>
+            <textarea
+              name="message"
+              id="message"
+              cols="70"
+              rows="10"
+              placeholder="Send me a message and I'll reply you as soon as possible..."
+              value={"" || value.message}
+              onChange={(e) => changeEvent(e.target)}
+            ></textarea>
+              <p className="error">
+              {errorMessage &&
+                errorMessage === "message" &&
+                `please enter a message`}
+            </p>
+          </div>
+        </div>
 
-            
-            <div className='agree'>
-               <input type="checkbox" name='agree' required/>
-               <label htmlFor="agree"> You agree to providing your data to owzbi who may contact you.</label>
-            </div>
-               <button type="submit" id='btn__submit'>Send Mail</button>
-        </form>
+        <div className="agree">
+          <input type="checkbox" name="agree" />
+          <label htmlFor="agree">
+            {" "}
+            You agree to providing your data to Segun who may contact you.
+          </label>
+        </div>
+        <button type="submit" id="btn__submit">
+          Send Mail
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
